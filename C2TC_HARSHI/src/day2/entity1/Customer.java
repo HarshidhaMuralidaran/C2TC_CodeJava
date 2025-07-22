5f63db8080de98c427a31b25ff67abd5ae319a0b
HARSHIDHA <HARSHIDHA@LAPTOP-GOEFA9UR>
2025-07-22 21:48:43 +0530

    Assingments

---
+++ C2TC_HARSHI/src/day2/entity1/Customer.java
@@ -0,0 +1,28 @@
+package day2.entity1;
+
+public class Customer {
+	private int cid;
+	private String cname;
+	private String city;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+
+}
---
+++ C2TC_HARSHI/src/day2/entity1/CustomerDemo.java
@@ -0,0 +1,21 @@
+package day2.entity1;
+
+public class CustomerDemo {
+	public static void main(Strinf[] args) {
+		Customer c1=new Customer();
+		
+		c1.setCid(101);
+		c1.setCname("Raja");
+		c1.setCity("Chennai");
+		System.out.println(c1);
+		Customer c2=new Customer();
+		c2.setCid(102);
+		c2.setCname("Lakshmi");
+		c2.setCity("Pondy");
+		System.out.println(c2);
+		
+		
+		
+	}
+
+}
