27c663be5ff476d663cbc8be40fc4ccd81783b21
HARSHIDHA <HARSHIDHA@LAPTOP-GOEFA9UR>
2025-08-04 09:40:52 +0530

    day12

---
+++ C2TC_HARSHI/src/day12/InvalidMarksException.java
@@ -0,0 +1,12 @@
+package day12;
+
+public class InvalidMarksException extends Executor {
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+
+}
